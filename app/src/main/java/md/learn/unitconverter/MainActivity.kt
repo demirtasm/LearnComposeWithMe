f0296993697630547879b1a9ab0639e2bc7ff88d
Mukaddes Demirtaş <m.ddemirtas@hotmail.com>
2024-03-18 13:01:51 +0300

    .

--- app/src/main/java/md/learn/unitconverter/MainActivity.kt
+++ app/src/main/java/md/learn/unitconverter/MainActivity.kt
@@ -59,11 +59,3 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
-
-/*
-IIn the contex of Android's Compose framework, the specific annotation directs the compiler to interpret a function declaratively.
-This approach signals the framework that the function declaratively. This approach signals he framework that he function is
-indeed to define a segment of the user interface, facilitating boot composition and recomposition processes.
-Composition refers to the initial rendering of the UI, while recomposition involves the dynamic redisplay orr recalibration
-of the UI's presentation based on changes in data or state. This mechanism ensures an efficient d responsive user interface in Android application
- */
